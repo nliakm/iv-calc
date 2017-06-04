adbecb3d3c7346be0d96c639c492991b268fadc7
Unknown <fuer.onlineanmeldungen@gmx.de>
2017-06-04 20:29:49 +0200

    removed scss folder

--- js/index.js
+++ js/index.js
@@ -48,6 +48,42 @@ $(document).ready(function() {
   setTimeout(skillSet, 1000);
 });
 
+function setTeamRating(element) {
+  if(element.id == 'team-blue' && $(element).is(":checked")) {
+    console.log("BLAAAAAAAAAAAAAAAAAA");
+    document.getElementById('team-selection').options[1].text = 'Dein (Pokémon Name) ist insgesamt einfach erstaunlich. Wirklich ein atemberaubendes Exemplar!';
+    document.getElementById('team-selection').options[2].text = 'Dein (Pokémon Name) ist mir insgesamt wirklich aufgefallen.';
+    document.getElementById('team-selection').options[3].text = 'Dein (Pokémon Name) ist insgesamt ein gutes Pokémon.';
+    document.getElementById('team-selection').options[4].text = 'Dein (Pokémon Name) wird im Kampf insgesamt wahrscheinlich keine großen Fortschritte machen.';
+
+    document.getElementById('iv-value').options[1].text = 'Seine Werte sind so hoch, dass ich sie gar nicht berechnen kann! Unglaublich!';
+    document.getElementById('iv-value').options[2].text = 'Ich muss sagen, dass ich von seinen Werten sehr beeindruckt bin.';
+    document.getElementById('iv-value').options[3].text = 'Seine Werte sind recht beeindruckend.';
+    document.getElementById('iv-value').options[4].text = 'Seine Werte sind insgesamt ganz okay.';
+  } else if(element.id == 'team-yellow' && $(element).is(":checked")) {
+    document.getElementById('team-selection').options[1].text = 'Dein (Pokémon Name) kann es im Kampf mit allen aufnehmen!';
+    document.getElementById('team-selection').options[2].text = 'Dein (Pokémon Name) ist insgesamt wirklich stark!';
+    document.getElementById('team-selection').options[3].text = 'Dein (Pokémon Name) ist insgesamt ganz gut.';
+    document.getElementById('team-selection').options[4].text = 'Dein (Pokémon Name) kann sich im Kampf noch verbessern!';
+
+    document.getElementById('iv-value').options[1].text = 'Seine Werte sind die besten, die ich je gesehen habe!';
+    document.getElementById('iv-value').options[2].text = 'Seine Werte sind wirklich hoch! Hut ab!';
+    document.getElementById('iv-value').options[3].text = 'Seine Werte können sich sehen lassen!';
+    document.getElementById('iv-value').options[4].text = 'Seine Werte sind okay.';
+  } else if(element.id == 'team-red' && $(element).is(":checked")) {
+    document.getElementById('team-selection').options[1].text = 'Von deinem (Pokémon Name) bin ich insgesamt total beeindruckt. Es kann einfach alles!';
+    document.getElementById('team-selection').options[2].text = 'Dein (Pokémon Name) ist insgesamt sehr stark. Du kannst stolz sein!';
+    document.getElementById('team-selection').options[3].text = 'Dein (Pokémon Name) ist insgesamt ganz gut!';
+    document.getElementById('team-selection').options[4].text = 'Dein (Pokémon Name) ist zwar insgesamt im Kampf nicht allzu beeindruckend, aber ich mag es trotzdem!';
+
+    document.getElementById('iv-value').options[1].text = 'Seine Werte hauen mich um. WOW!';
+    document.getElementById('iv-value').options[2].text = 'Er hat hervorragende Werte! Wie aufregend!';
+    document.getElementById('iv-value').options[3].text = 'Seine Werte sind nicht von schlechten Eltern!';
+    document.getElementById('iv-value').options[4].text = 'Seine Werte werden dir im Kampf nicht viel bringen.';
+  } else {
+  }
+}
+
 function openMoveInfos(element) {
   if(element.id == "quick-move-1") {
     if(document.getElementById("quick-move-info-1").style.display == "none") {
@@ -58,31 +94,12 @@ function openMoveInfos(element) {
   } else if(element.id == "quick-move-2") {
     if(document.getElementById("quick-move-info-2").style.display == "none") {
       document.getElementById("quick-move-info-2").style.display = "block";
-    } else {
+    }  else {
       document.getElementById("quick-move-info-2").style.display = "none";
-    }  }
+    }
 }
 
-function setTeamRating(element) {
-  if(element.id == 'team-blue' && $(element).is(":checked")) {
-    document.getElementById('team-selection').options[1].text = 'Dein (Pokémon Name) ist insgesamt einfach erstaunlich. Wirklich ein atemberaubendes Exemplar!';
-    document.getElementById('team-selection').options[2].text = 'Dein (Pokémon Name) ist mir insgesamt wirklich aufgefallen.';
-    document.getElementById('team-selection').options[3].text = 'Dein (Pokémon Name) ist insgesamt ein gutes Pokémon.';
-    document.getElementById('team-selection').options[4].text = 'Dein (Pokémon Name) wird im Kampf insgesamt wahrscheinlich keine großen Fortschritte machen.';
-  } else if(element.id == 'team-yellow' && $(element).is(":checked")) {
-    document.getElementById('team-selection').options[1].text = 'Dein (Pokémon Name) kann es im Kampf mit allen aufnehmen!';
-    document.getElementById('team-selection').options[2].text = 'Dein (Pokémon Name) ist insgesamt wirklich stark!';
-    document.getElementById('team-selection').options[3].text = 'Dein (Pokémon Name) ist insgesamt ganz gut.';
-    document.getElementById('team-selection').options[4].text = 'Dein (Pokémon Name) kann sich im Kampf noch verbessern!';
-  } else if(element.id == 'team-red' && $(element).is(":checked")) {
-    document.getElementById('team-selection').options[1].text = 'Von deinem (Pokémon Name) bin ich insgesamt total beeindruckt. Es kann einfach alles!';
-    document.getElementById('team-selection').options[2].text = 'Dein (Pokémon Name) ist insgesamt sehr stark. Du kannst stolz sein!';
-    document.getElementById('team-selection').options[3].text = 'Dein (Pokémon Name) ist insgesamt ganz gut!';
-    document.getElementById('team-selection').options[4].text = 'Dein (Pokémon Name) ist zwar insgesamt im Kampf nicht allzu beeindruckend, aber ich mag es trotzdem!';
-  }
-}
-
-function showTrainerBew(element) {
+function showTrainerRating(element) {
   if(element.id == 'checkbox-att') {
     var elem = document.getElementById('trainer-att');
   } else if(element.id == 'checkbox-def') {
@@ -94,3 +111,39 @@ function showTrainerBew(element) {
       elem.style.display = this.checked ? 'block' : 'none';
     };
 }
+
+/**
+function setTeamRating(element) {
+  if(element.id == 'team-blue' && $(element).is(":checked")) {
+    document.getElementById('team-selection').options[1].text = 'Dein (Pokémon Name) ist insgesamt einfach erstaunlich. Wirklich ein atemberaubendes Exemplar!';
+    document.getElementById('team-selection').options[2].text = 'Dein (Pokémon Name) ist mir insgesamt wirklich aufgefallen.';
+    document.getElementById('team-selection').options[3].text = 'Dein (Pokémon Name) ist insgesamt ein gutes Pokémon.';
+    document.getElementById('team-selection').options[4].text = 'Dein (Pokémon Name) wird im Kampf insgesamt wahrscheinlich keine großen Fortschritte machen.';
+
+    document.getElementById('iv-value').options[1].text = 'Seine Werte sind so hoch, dass ich sie gar nicht berechnen kann! Unglaublich!';
+    document.getElementById('iv-value').options[2].text = 'Ich muss sagen, dass ich von seinen Werten sehr beeindruckt bin.';
+    document.getElementById('iv-value').options[3].text = 'Seine Werte sind recht beeindruckend.';
+    document.getElementById('iv-value').options[4].text = 'Seine Werte sind insgesamt ganz okay.';
+  } else if(element.id == 'team-yellow' && $(element).is(":checked")) {
+    document.getElementById('team-selection').options[1].text = 'Dein (Pokémon Name) kann es im Kampf mit allen aufnehmen!';
+    document.getElementById('team-selection').options[2].text = 'Dein (Pokémon Name) ist insgesamt wirklich stark!';
+    document.getElementById('team-selection').options[3].text = 'Dein (Pokémon Name) ist insgesamt ganz gut.';
+    document.getElementById('team-selection').options[4].text = 'Dein (Pokémon Name) kann sich im Kampf noch verbessern!';
+
+    document.getElementById('iv-value').options[1].text = 'Seine Werte sind die besten, die ich je gesehen habe!';
+    document.getElementById('iv-value').options[2].text = 'Seine Werte sind wirklich hoch! Hut ab!';
+    document.getElementById('iv-value').options[3].text = 'Seine Werte können sich sehen lassen!';
+    document.getElementById('iv-value').options[4].text = 'Seine Werte sind okay.';
+  } else if(element.id == 'team-red' && $(element).is(":checked")) {
+    document.getElementById('team-selection').options[1].text = 'Von deinem (Pokémon Name) bin ich insgesamt total beeindruckt. Es kann einfach alles!';
+    document.getElementById('team-selection').options[2].text = 'Dein (Pokémon Name) ist insgesamt sehr stark. Du kannst stolz sein!';
+    document.getElementById('team-selection').options[3].text = 'Dein (Pokémon Name) ist insgesamt ganz gut!';
+    document.getElementById('team-selection').options[4].text = 'Dein (Pokémon Name) ist zwar insgesamt im Kampf nicht allzu beeindruckend, aber ich mag es trotzdem!';
+
+    document.getElementById('iv-value').options[1].text = 'Seine Werte hauen mich um. WOW!';
+    document.getElementById('iv-value').options[2].text = 'Er hat hervorragende Werte! Wie aufregend!';
+    document.getElementById('iv-value').options[3].text = 'Seine Werte sind nicht von schlechten Eltern!';
+    document.getElementById('iv-value').options[4].text = 'Seine Werte werden dir im Kampf nicht viel bringen.';
+  }
+}
+*/
